cb161f8172a832cc3d508ab19f822e7347546086
MarliesR <marlies.rieder95@gmail.com>
2022-05-20 13:19:54 +0200

    Add project files.

---
+++ TourPlanner/TourPlanner.DAL.Mapquest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TourPlanner.DAL.Mapquest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TourPlanner/TourPlanner.DAL.SQL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TourPlanner.DAL.SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
