d7d5bec0995376136c09214a430cc4c94d7346b7
MarliesR <marlies.rieder95@gmail.com>
2022-05-22 12:25:55 +0200

    add tour sql

---
+++ TourPlanner/TourPlanner.DAL.SQL/TourSql.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.DAL.SQL
+{
+    class TourSql
+    { 
+        //hier alle sql statements zur tour eingeben? 
+    }
+}
