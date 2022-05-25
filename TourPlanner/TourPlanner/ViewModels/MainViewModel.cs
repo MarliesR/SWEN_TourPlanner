32ebfb5cc8f2cec75dfc3aa1b1c536e6cb51e969
MarliesR <marlies.rieder95@gmail.com>
2022-05-25 11:06:28 +0200

    navigation and adding tour works

--- TourPlanner/TourPlanner/ViewModels/MainViewModel.cs
+++ TourPlanner/TourPlanner/ViewModels/MainViewModel.cs
@@ -11,16 +11,30 @@ namespace TourPlanner.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        public object selectedViewModel; //DATA binding mit dem MainWindow
+        public object SelectedViewModel  //schaut ob sich die value des viewmodels ändert
+        {
+            get => selectedViewModel;
+            set
+            {
+                if (selectedViewModel != value)
+                {
+                    selectedViewModel = value;
+                    RaisePropertyChangedEvent(nameof(SelectedViewModel));
+                }
+            }
+        }
 
+
+        //wurde automatisch generiert
         private RelayCommand showTourPageCommand1;
         public ICommand showTourPageCommand => showTourPageCommand1 ??= new RelayCommand(showTourPage);
 
         private void showTourPage(object commandParameter)
         {
-            AddTourView view = new ();
+            SelectedViewModel = new AddTourViewModel();
+        }
 
-
-        }        
         
         //https://github.com/BernLeWal/fhtw-bif4-layeredarchitecture-cs 
         //viewmodelbase ist vorgegeben
--- TourPlanner/TourPlanner/Views/MainWindow.xaml.cs
+++ TourPlanner/TourPlanner/Views/MainWindow.xaml.cs
@@ -13,26 +13,7 @@ namespace TourPlanner
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new MainViewModel();
         }
 
-        private void TourList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            mainView.Content = new EditTourView();
-            // tour name und id müssen irgendwie verlinkt sein
-        }
-
-        private void Button_Click_AddNewTour(object sender, RoutedEventArgs e)
-        {
-            //mainView.Content = new AddTourView();
-            mainView.NavigationService.Navigate(new AddTourView());
-
-            //string TourName = AddTourView.GetTourName();
-            //if (TourName != null)
-            //{
-            //    TourList.Items.Add(TourName);
-            //}
-
-        }
     }
 }
