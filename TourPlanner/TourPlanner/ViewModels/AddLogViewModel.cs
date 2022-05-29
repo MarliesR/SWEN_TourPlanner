1ff7c9311b201db56f0d71b7d0dca53f5f0a9f42
MarliesR <marlies.rieder95@gmail.com>
2022-05-29 12:37:09 +0200

    Add log, show all logs

--- TourPlanner/TourPlanner/ViewModels/AddLogViewModel.cs
+++ TourPlanner/TourPlanner/ViewModels/AddLogViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using TourPlanner.Library;
+using TourPlanner.BL;
+using TourPlanner.ViewModels;
 
 namespace TourPlanner.ViewModels
 {
@@ -12,13 +16,46 @@ namespace TourPlanner.ViewModels
     {
         private Window currentWindow;
         private string tourName;
+        private string logDate = "2022-01-01";
+        private string logTimeTotal = "00:00";
+        private int logRating = 1;
+        public ObservableCollection<int> RatingTypes { get; set; }
+        private int logDifficulty = 1;
+        public ObservableCollection<int> DifficultyTypes { get; set; }
+        private string logComment;
+        private int tourid;
+
 
         public AddLogViewModel(Window window, Tour tour)
         {
+            currentWindow = window;
             tourName = tour.Name;
+            tourid = tour.Id;
+            RatingTypes = new ObservableCollection<int>();
+            DifficultyTypes = new ObservableCollection<int>();
+            initializeRatingTypes();
+            initializeDifficultyTypes();
+
         }
 
-        public String TourName //das ist der name der im binding angegeben ist im view von add tour
+        private void initializeRatingTypes()
+        {
+            RatingTypes.Add(1);
+            RatingTypes.Add(2);
+            RatingTypes.Add(3);
+            RatingTypes.Add(4);
+            RatingTypes.Add(5);
+        }
+
+        private void initializeDifficultyTypes()
+        {
+            DifficultyTypes.Add(1);
+            DifficultyTypes.Add(2);
+            DifficultyTypes.Add(3);
+            DifficultyTypes.Add(4);
+            DifficultyTypes.Add(5);
+        }
+        public String TourName
         {
             get => tourName;
             set
@@ -29,6 +66,95 @@ namespace TourPlanner.ViewModels
                     RaisePropertyChangedEvent(nameof(TourName));
                 }
             }
+        }
+        public String LogDate
+        {
+            get => logDate;
+            set
+            {
+                if (logDate != value)
+                {
+                    logDate = value;
+                    RaisePropertyChangedEvent(nameof(LogDate));
+                }
+            }
+        }
+
+        public String LogTimeTotal
+        {
+            get => logTimeTotal;
+            set
+            {
+                if (logTimeTotal != value)
+                {
+                    logTimeTotal = value;
+                    RaisePropertyChangedEvent(nameof(LogTimeTotal));
+                }
+            }
+        }
+
+        public int LogRating
+        {
+            get => logRating;
+            set
+            {
+                if (logRating != value)
+                {
+                    logRating = value;
+                    RaisePropertyChangedEvent(nameof(LogRating));
+                }
+            }
+        }
+
+        public int LogDifficulty
+        {
+            get => logDifficulty;
+            set
+            {
+                if (logDifficulty != value)
+                {
+                    logDifficulty = value;
+                    RaisePropertyChangedEvent(nameof(LogDifficulty));
+                }
+            }
+        }
+
+        public String LogComment
+        {
+            get => logComment;
+            set
+            {
+                if (logComment != value)
+                {
+                    logComment = value;
+                    RaisePropertyChangedEvent(nameof(LogComment));
+                }
+            }
+        }
+
+
+
+        private RelayCommand clearLogCommand;
+        public ICommand ClearLogCommand => clearLogCommand ??= new RelayCommand(ClearLog);
+
+        private void ClearLog(object commandParameter)
+        {
+            LogDate = "2022-01-01";
+            LogComment = string.Empty;
+            LogDifficulty = 1;
+            LogRating = 1;
+            LogTimeTotal = "00:00";
+        }
+
+        private RelayCommand saveLogCommand;
+        public ICommand SaveLogCommand => saveLogCommand ??= new RelayCommand(SaveLog);
+
+        private void SaveLog(object commandParameter)
+        {
+            TourLog log = new TourLog(tourid, logDate, logComment, logDifficulty, logTimeTotal, logRating);
+            TourHandler handler = new TourHandler();
+            handler.AddLog(log);
+            currentWindow.Close();
         }
     }
 }
